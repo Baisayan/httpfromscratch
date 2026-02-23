bcfcea9de79448087910d68d3e803b66c2677447
Ved <bbbbaisayan@gmail.com>
2026-02-23 19:31:35 +0530

    used it to create a channel which reads 8bytes char everytime and joins them into line

--- main.go
+++ main.go
@@ -1,22 +1,52 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	out := make(chan string, 1)
+
+	go func() {
+		defer close(out)
+		defer f.Close()
+		str := ""
+		for {
+			data := make([]byte, 8)
+			n, err := f.Read(data)
+			if err != nil {
+				break
+			}
+			data = data[:n]
+			if i := bytes.IndexByte(data, '\n'); i != -1 {
+				str += string(data[:i])
+				data = data[i+1:]
+				out <- str
+				str = ""
+			}
+			str += string(data)
+		}
+
+		if len(str) != 0 {
+			out <- str
+		}
+	}()
+
+	return out
+}
 
 func main() {
 	f, err := os.Open("msg.txt")
 	if err != nil {
 		log.Fatal("error", "error", err)
 	}
-	for {
-		data := make([]byte, 8)
-		n, err := f.Read(data)
-		if err != nil {
-			break
-		}
-		fmt.Printf("read: %s\n", string(data[:n]))
+
+	lines := getLinesChannel(f)
+	for line := range lines {
+		fmt.Println(line)
 	}
 }
